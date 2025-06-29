665b5194ab088829ded164aa3e2a18bcb59079bc
CBeeney <C.Beeney@outlook.com>
2025-06-29 15:24:18 +0100

    Added fork syscall.

--- Header/progman.h
+++ Header/progman.h
@@ -7,3 +7,8 @@ enum EXIT {
 // e.g. exit(0);
 // The above line would exit the program with an exit code of 0, indicating success.
 void exit (int rc);
+
+// Duplicates the running process
+// e.g. fork();
+// The above line would duplicate the current process into 2.
+void fork();
