9a99818adb14155c44d29bdcc6277f8edfd3c664
CBeeney <C.Beeney@outlook.com>
2025-06-29 01:48:54 +0100

    Did something

--- Header/conio.h
+++ Header/conio.h
@@ -1,7 +1,7 @@
 enum CONTYPE
 {
-    STDOUT,
     STDIN,
+    STDOUT,
     STDERR
 };
 
@@ -9,3 +9,8 @@ enum CONTYPE
 // e.g. write(stdout, "Hello, World!\n", strlen("Hello, World!\n"));
 // The above line would print Hello, World! to the console under typical circumstances.
 void write (int fd, void *buf, int count);
+
+// Reads count bytes of fd to buf
+// e.g. read(stdin,my_buf,256);
+// The above line reads <=256 chars from console to my_buf under typical circumstances.
+int read (int fd, void *buf, int count);
